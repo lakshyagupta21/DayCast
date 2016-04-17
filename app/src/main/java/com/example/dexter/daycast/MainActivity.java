45bc5d2225f35e8f6179cfcb30cbb49080300fd6
lovish888 <lovish79214@gmail.com>
2016-04-17 14:29:06 +0530

    Basic layout containing weather details created.

--- app/src/main/java/com/example/dexter/daycast/MainActivity.java
+++ app/src/main/java/com/example/dexter/daycast/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.dexter.daycast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView location, temperature, humidity, weatherStatus, lastUpdated;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat format;
+        format = new SimpleDateFormat("E MMM dd hh:mm:ss a");
+        String time = String.format("Last Updated: "+format.format(calendar.getTime()));
+
+        location = (TextView) findViewById(R.id.location);
+        temperature = (TextView) findViewById(R.id.temperature);
+        humidity = (TextView) findViewById(R.id.humidity);
+        weatherStatus = (TextView) findViewById(R.id.status);
+        lastUpdated = (TextView) findViewById(R.id.last_updated);
+        lastUpdated.setText(time);
+
     }
 }
